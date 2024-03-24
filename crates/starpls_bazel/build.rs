5b4b69cfaec6ca39aed12d0228e3d9dd6c7889bc
withered-magic <withering.magic@gmail.com>
2024-03-24 12:05:50 -0700

    move proto

--- crates/starpls_bazel/build.rs
+++ crates/starpls_bazel/build.rs
@@ -1,9 +1,6 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(
-        &["src/builtin.proto", "data/build.proto"],
-        &["data/", "src/"],
-    )?;
+    prost_build::compile_protos(&["data/builtin.proto", "data/build.proto"], &["data/"])?;
     Ok(())
 }

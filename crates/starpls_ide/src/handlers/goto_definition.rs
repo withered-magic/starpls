57c85cf8a5a1b6009004c5f866e34494dbe1462b
withered-magic <153045167+withered-magic@users.noreply.github.com>
2024-03-29 23:37:39 -0700

    Support goto for labels in load() statements (#120)

--- crates/starpls_ide/src/handlers/goto_definition.rs
+++ crates/starpls_ide/src/handlers/goto_definition.rs
@@ -3,13 +3,14 @@ use starpls_common::{parse, Db};
 use starpls_hir::{Name, Semantics};
 use starpls_syntax::{
     ast::{self, AstNode},
-    T,
+    TextRange, TextSize, T,
 };
 
 pub(crate) fn goto_definition(
     db: &Database,
     FilePosition { file_id, pos }: FilePosition,
 ) -> Option<Vec<Location>> {
+    let sema = Semantics::new(db);
     let file = db.get_file(file_id)?;
     let parse = parse(db, file);
     let parent = pick_best_token(parse.syntax(db).token_at_offset(pos), |kind| match kind {
@@ -20,13 +21,11 @@ pub(crate) fn goto_definition(
     })?
     .parent()?;
 
-    // For now, we only handle identifiers.
-    if let Some(name_ref) = ast::NameRef::cast(parent) {
+    if let Some(name_ref) = ast::NameRef::cast(parent.clone()) {
         let name = Name::from_ast_node(name_ref.clone());
-        let sema = Semantics::new(db);
         let scope =
             sema.scope_for_expr(file, &ast::Expression::cast(name_ref.syntax().clone())?)?;
-        return Some(
+        Some(
             scope
                 .resolve_name(&name)?
                 .into_iter()
@@ -37,7 +36,15 @@ pub(crate) fn goto_definition(
                     })
                 })
                 .collect(),
-        );
+        )
+    } else if let Some(load_module) = ast::LoadModule::cast(parent) {
+        let load_stmt = ast::LoadStmt::cast(load_module.syntax().parent()?)?;
+        let file = sema.resolve_load_stmt(file, &load_stmt)?;
+        Some(vec![Location {
+            file_id: file.id(db),
+            range: TextRange::new(TextSize::new(0), TextSize::new(1)),
+        }])
+    } else {
+        None
     }
-    None
 }

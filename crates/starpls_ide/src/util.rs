f69c5af01b9e35ca10a1d96d71088bbbde5486a2
withered-magic <withering.magic@gmail.com>
2024-04-04 03:12:45 -0700

    fix unindent logic for code blocks

--- crates/starpls_ide/src/util.rs
+++ crates/starpls_ide/src/util.rs
@@ -10,13 +10,20 @@ pub(crate) fn pick_best_token(
 // TODO(withered-magic): This logic should probably be more sophisticated, but it works well
 // enough for now.
 pub(crate) fn unindent_doc(doc: &str) -> String {
+    let mut is_in_code_block = false;
     unindent::unindent(doc)
         .lines()
         .map(|line| {
             let trimmed = line.trim_start();
             let num_trimmed = line.len() - trimmed.len();
             let mut s = String::new();
-            (0..num_trimmed).for_each(|_| s.push_str("&nbsp;"));
+
+            if trimmed.starts_with("```") {
+                is_in_code_block = !is_in_code_block;
+            }
+
+            (0..num_trimmed)
+                .for_each(|_| s.push_str(if is_in_code_block { " " } else { "&nbsp;" }));
             s.push_str(trimmed);
             s.push_str("  ");
             s
